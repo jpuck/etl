1b81e4f201f789f12d61a440e097b842e70b0d36
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-04 17:09:18 -0500

    validate raw JSON input

--- src/Data/JSON.php
+++ src/Data/JSON.php
@@ -1,8 +1,16 @@
 <?php
 namespace jpuck\etl\Data;
+use InvalidArgumentException;
 
 class JSON extends Datum {
 	protected function parse($raw) : Array {
-		return [];
+		$json = json_decode($raw, true);
+		if (!is_array($json)){
+			throw new InvalidArgumentException(
+				"Bad JSON: ".json_last_error_msg()
+			);
+		}
+
+		return $json;
 	}
 }

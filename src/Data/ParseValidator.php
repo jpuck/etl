4267e937e110c9d83e4b62ea4e67ea794dd16038
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-04 14:48:48 -0500

    validate parsed data has name index

--- src/Data/ParseValidator.php
+++ src/Data/ParseValidator.php
@@ -1,6 +1,14 @@
 <?php
 namespace jpuck\etl\Data;
+use InvalidArgumentException;
 
 class ParseValidator {
-	public function validate(){}
+	public function validate(Array $array) : Bool {
+		if (!array_key_exists('name',$array)){
+			throw new InvalidArgumentException(
+				'Array must have "name" index.', 1
+			);
+		}
+		return true;
+	}
 }

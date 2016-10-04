61858d949c23f7cf7da9891a6e6b136317e1e3d4
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-04 16:17:37 -0500

    optionally validate parse in Datum constructor

--- src/Data/Datum.php
+++ src/Data/Datum.php
@@ -3,26 +3,36 @@ namespace jpuck\etl\Data;
 
 use jpuck\etl\Schemata\Schema;
 use jpuck\etl\Schemata\Schematizer;
+use jpuck\etl\Data\ParseValidator;
 
 abstract class Datum {
 	protected $raw;
 	protected $parsed;
 	protected $schema;
+	protected $validate_parse = true;
 
-	public function __construct($raw, Schema $override=null){
+	public function __construct($raw, Schema $override=null, Bool $validate_parse=true){
+		$this->validate_parse = $validate_parse;
 		$this->raw($raw, $override);
 	}
 
 	public function raw($raw=null, Schema $override=null){
 		if (isset($raw)){
 			$this->parsed = $this->parse($raw);
+
+			if ($this->validate_parse){
+				(new ParseValidator)->validate($this->parsed);
+			}
+
 			$this->raw = $raw;
+
 			if (isset($override)){
 				$this->schema($override);
 			} else {
 				$this->schema((new Schematizer)->schematize($this));
 			}
 		}
+
 		return $this->raw;
 	}
 
